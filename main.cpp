2e1c6a2008b3e0bd3332824b0a98f2c20f008417
Victor Hernandez <vichdz97@txstate.edu>
2020-03-03 20:08:11 -0600

    updated main

--- main.cpp
+++ main.cpp
@@ -1,14 +1,23 @@
-// makes calls to forward and backward, does nothing else
+#include "forward.cpp"
+#include "backward.cpp"
 
 #include <iostream>
 using namespace std;
 
-int main ()
+int main()
 {
-    cout << "Hello, World!" << endl;
+    cout<< "Thank you for using our program to determine your major and area."<<endl
+        << "The program will first tell you your major, and then it will suggest an area of study to you"<<endl
+        <<"Please answer all quetions to the best of your ability."
+        << endl<< endl;
+
+    int back;
+    int forw;
+
+    back = backward();
+    forw = forward();
+
+    cout << endl << endl << "The program has now completed. Thank you." <<endl;
+
     return 0;
 }
-//
-// Created by lowor on 3/1/20.
-//
-
